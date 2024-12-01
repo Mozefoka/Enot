5c1d50a5c44cf1456200bdfd7864be7a92b3f643
Mozefoka <mozefoka@gmail.com>
2024-12-01 17:59:41 +0300

    fixes

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ const line3 = document.querySelector(".header__burger-line3");
 const menu = document.querySelector(".header__menu");
 const promotions = document.querySelector(".header__menu-list-promotions-btn");
 const promotionsItem = document.querySelector(".header__menu-list-promotions-item");
+const questionsItem = document.querySelectorAll(".questions__list-item");
+const questionsTitle = document.querySelector(".questions__list-title");
+const questionsText = document.querySelector(".questions__list-text");
 const filterForm = document.querySelector(".floor-covering__filter");
 const filterReset = document.querySelector(".floor-covering__filter-reset");
 const infoPopup = document.querySelector(".commodity__info-popup-wrapper");
+let buttonCountPlus = document.getElementById("buttonCountPlus");
+let buttonCountMinus = document.getElementById("buttonCountMinus");
+let count = document.getElementById("buttonCountNumber");
+let count2 = document.getElementById("num");
+let number = 1;
+let price = 2500;
+
+buttonCountPlus.onclick = function() {
+        number++;
+        count.innerHTML = number;
+        count2.value = number * price;
+};
+
+buttonCountMinus.onclick = function() {
+   if (number >= 2) {
+       number--;
+       count.innerHTML = number;
+       count2.value = number * price;
+    }
+};
 
 burger.addEventListener('click', function() {
 	burger.classList.toggle("header__burger--active");
