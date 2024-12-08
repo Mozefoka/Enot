92c1e8528d95b1f789f806acfb73f646847b9902
Mozefoka <mozefoka@gmail.com>
2024-12-08 15:56:13 +0300

    nope

--- commodity.js
+++ commodity.js
@@ -8,42 +8,49 @@ const buttonCountPlusArea = document.getElementById("buttonCountPlusArea");
 const buttonCountMinusArea = document.getElementById("buttonCountMinusArea");
 const countArea = document.getElementById("buttonCountNumberArea");
 const area = 0.98;
+const price = document.getElementById("price");
+price.setAttribute('sum', 2500);
+const priceSum = price.getAttribute('sum');
 
 countArea.addEventListener('change', (event) => {
     event.target.value = (Math.round(event.target.value / area) * area).toFixed(2);
-    count.value = event.target.value / area;
+    count.value = Math.round(event.target.value / area);
+    price.innerHTML = +priceSum * +count.value;
 });
 
 count.addEventListener('change', (event) => {
     event.target.value = Math.round(event.target.value);
     countArea.value = event.target.value * area;
+    price.innerHTML = event.target.value * priceSum;
 });
 
 buttonCountPlusArea.addEventListener('click', () => {
-    countArea.value = +countArea.value + area;
+    countArea.value = (+countArea.value + area).toFixed(2);
     count.value = +count.value + 1;
+    price.innerHTML = +price.innerHTML + +priceSum;
 });
 
 buttonCountMinusArea.addEventListener('click', () => {
    if (countArea.value >= 1) {
-    countArea.value = +countArea.value - area;
+    countArea.value = (+countArea.value - area).toFixed(2);
     count.value = +count.value - 1;
+    price.innerHTML = +price.innerHTML - +priceSum;
     }
 });
 
 buttonCountPlus.addEventListener('click', () => {
     count.value = +count.value + 1;
-    countArea.value = +countArea.value + area;
+    countArea.value = (+countArea.value + area).toFixed(2);
+    price.innerHTML = +price.innerHTML + +priceSum;
 });
 
 buttonCountMinus.addEventListener('click', () => {
 if (count.value >= 2) {
     count.value = +count.value - 1;
-    countArea.value = +countArea.value - area;
+    countArea.value = (+countArea.value - area).toFixed(2);
+    price.innerHTML = +price.innerHTML - +priceSum;
 }
 });
-console.log(countArea.value);
-console.log(countArea.attributes);
 
 function popup() {
 	infoPopup.classList.toggle("commodity__info-popup-wrapper--active");
